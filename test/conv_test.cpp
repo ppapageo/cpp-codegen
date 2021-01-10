3b8f99b5e81f05b763234dd83b9d9f765bf65c32
Panagiotis Papageorgiou <pkpapageorg@gmail.com>
2021-01-10 14:03:55 +0200

    added more tests for conv example

--- test/conv_test.cpp
+++ test/conv_test.cpp
@@ -38,6 +38,51 @@ TEST(Conv, CgenConv) {
 	VanillaConv vc(out_ch, in_ch, in_row, in_col, k_row, k_col, pad, stride, input, kernel);
 	CgenConv cc(out_ch, in_ch, in_row, in_col, k_row, k_col, pad, stride, input, kernel);
 	EXPECT_EQ(true,matr_all_equal(vc.output,cc.output,vc.out_ch*vc.out_col*vc.out_row));
+	delete [] input;
+	delete [] kernel;
+}
 
+TEST(Conv, CgenUnrollConv) {
+	int in_ch=32;
+	int in_row=96;
+	int in_col=96;
+	int out_ch=128;
+	int k_row=3;
+	int k_col=3;
+	int stride=1;
+	int pad=1;
+	if (pad==1){
+		pad=(k_row-1)/2;
+	}
+	float* input = populate_rand(in_ch*in_row*in_col, 15.42, 0.42);
+	float* kernel = populate_rand(out_ch*in_ch*k_row*k_col, 15.42, 0.42);
+	pad_input(in_ch, &in_row, &in_col, pad, &input);	
+	VanillaConv vc(out_ch, in_ch, in_row, in_col, k_row, k_col, pad, stride, input, kernel);
+	CgenUnrollConv uc(out_ch, in_ch, in_row, in_col, k_row, k_col, pad, stride, input, kernel);
+	EXPECT_EQ(true,matr_all_equal(vc.output,uc.output,vc.out_ch*vc.out_col*vc.out_row));
+	delete [] input;
+	delete [] kernel;
+}
+
+TEST(Conv, Im2colConv) {
+	int in_ch=32;
+	int in_row=96;
+	int in_col=96;
+	int out_ch=128;
+	int k_row=3;
+	int k_col=3;
+	int stride=1;
+	int pad=1;
+	if (pad==1){
+		pad=(k_row-1)/2;
+	}
+	float* input = populate_rand(in_ch*in_row*in_col, 15.42, 0.42);
+	float* kernel = populate_rand(out_ch*in_ch*k_row*k_col, 15.42, 0.42);
+	pad_input(in_ch, &in_row, &in_col, pad, &input);	
+	VanillaConv vc(out_ch, in_ch, in_row, in_col, k_row, k_col, pad, stride, input, kernel);
+	Im2colConv ic(out_ch, in_ch, in_row, in_col, k_row, k_col, pad, stride, input, kernel);
+	EXPECT_EQ(true,matr_all_equal(vc.output,ic.output,vc.out_ch*vc.out_col*vc.out_row));
+	delete [] input;
+	delete [] kernel;
 }
 
